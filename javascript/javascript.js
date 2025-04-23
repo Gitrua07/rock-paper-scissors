a06830774694ca4d0266308896a8e3a25558e6ed
Gaia Iturralde <138412962+Gitrua07@users.noreply.github.com>
2025-04-22 22:55:40 -0700

    Implemented getComputerChoice:

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,23 +2,43 @@
 //create a variable called computerScore initialized to 0
 
 //create a function called getComputerChoice
+    function getComputerChoice(){
+
     //create a variable called computer_action
+    let computer_action;
+
     //generate a number between 0 to 2
     //insert into the variable random_action
+    let random_action = Math.floor(Math.random() * 3) + 1;
+    console.log(random_action);
     //if random_number is 0:
+    if (random_action == 1){
         //assign the value "rock" to computer_action
+        computer_action = "Rock";
     //else if random_number is 1:
+    }else if(random_action == 2){
         //assign the value "paper" to computer_action
+        computer_action = "Paper";
     //else if random_number is 2:
+    }else{
         //assign the value "scissors" to computer_action
+        computer_action = "Scissors";
+    }
+
     //return computer_action value
+    return computer_action;
+
+    }
 
 //create a function called getHumanChoice
+    function getHumanChoice(){
     //prompt user to input an answer into human_action
     //return human_action value
+    }
 
 //create a function called playRound which has two
 // parameters humanChoice and computerChoice
+    function playRound(humanChoice, computerChoice){
     //make humanChoice and computerChoice all lowercase with uppercase first letter
     //create a variable called winner
     // if computerChoice is rock and humanChoice is scissors
@@ -36,10 +56,15 @@
     //if winner is computerChoice
         //output "You lose! 'computerChoice beats 'humanChoice'."
         //computerScore is incremented
+    }
 
 //creates a function called playGame
+    function playGame(){
     //loop five times
         //run getComputerChoice
         //run getHumanChoice
         //call playRound
+    }
+
+    console.log(getComputerChoice());
 
