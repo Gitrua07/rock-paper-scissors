89dd6bdb0b944d3dfcd964db4a003f24bac92ad2
Gaia Iturralde <138412962+Gitrua07@users.noreply.github.com>
2025-04-22 23:49:05 -0700

    Implemented all functions

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,74 +1,115 @@
-//create a variable called humanScore initialized to 0
-//create a variable called computerScore initialized to 0
-
 //create a function called getComputerChoice
-    function getComputerChoice(){
+function getComputerChoice() {
 
     //create a variable called computer_action
-    let computer_action;
+    let computerAction;
 
     //generate a number between 0 to 2
     //insert into the variable random_action
-    let random_action = Math.floor(Math.random() * 3) + 1;
-    console.log(random_action);
+    let randomAction = Math.floor(Math.random() * 3) + 1;
+
     //if random_number is 0:
-    if (random_action == 1){
+    if (randomAction == 1) {
         //assign the value "rock" to computer_action
-        computer_action = "Rock";
-    //else if random_number is 1:
-    }else if(random_action == 2){
+        computerAction = "Rock";
+        //else if random_number is 1:
+    } else if (randomAction == 2) {
         //assign the value "paper" to computer_action
-        computer_action = "Paper";
-    //else if random_number is 2:
-    }else{
+        computerAction = "Paper";
+        //else if random_number is 2:
+    } else {
         //assign the value "scissors" to computer_action
-        computer_action = "Scissors";
+        computerAction = "Scissors";
     }
 
     //return computer_action value
-    return computer_action;
+    return computerAction;
 
-    }
+}
 
 //create a function called getHumanChoice
-    function getHumanChoice(){
-    
-    //prompt user to input an answer into human_action
-    let human_action = prompt("CHOOSE: Rock, Paper, Scissors");
-    
-    //return human_action value
-    return human_action;
-    }
+function getHumanChoice() {
 
-//create a function called playRound which has two
-// parameters humanChoice and computerChoice
-    function playRound(humanChoice, computerChoice){
-    //make humanChoice and computerChoice all lowercase with uppercase first letter
-    //create a variable called winner
-    // if computerChoice is rock and humanChoice is scissors
-    // OR
-    // if computerChoice is paper and humanChoice is rock
-    // OR
-    // if computerChoice is scissors and humanChoice is paper
-        //winner is computerChoice
-    //else if computerChoice is not equal to humanChoice
-        //winner is humanChoice
-    
-    //if winner is humanChoice
-        //output "You win! 'humanChoice' beats 'computerChoice'."
-        //humanScore is incremented
-    //if winner is computerChoice
-        //output "You lose! 'computerChoice beats 'humanChoice'."
-        //computerScore is incremented
-    }
+    //prompt user to input an answer into human_action
+    let humanAction = prompt();
+
+    //return human_action value
+    return humanAction;
+}
 
 //creates a function called playGame
-    function playGame(){
-    //loop five times
-        //run getComputerChoice
-        //run getHumanChoice
-        //call playRound
+function playGame() {
+    //create a variable called humanScore initialized to 0
+    let humanScore = 0;
+
+    //create a variable called computerScore initialized to 0
+    let computerScore = 0;
+
+    //create a function called playRound which has two
+    // parameters humanChoice and computerChoice
+    function playRound(humanChoice, computerChoice) {
+        //make humanChoice and computerChoice all lowercase with uppercase first letter
+        let humanAction;
+        let computerAction;
+        let lowerCaseLetter = humanChoice.toLowerCase();
+        let firstLetter = humanChoice[0].toUpperCase();
+        let sliceLetter = lowerCaseLetter.slice(1);
+        humanAction = firstLetter + sliceLetter;
+
+        lowerCaseLetter = computerChoice.toLowerCase();
+        firstLetter = computerChoice[0].toUpperCase();
+        sliceLetter = lowerCaseLetter.slice(1);
+        computerAction = firstLetter + sliceLetter;
+
+        //create a variable called winner
+        let winner;
+
+        // if computerChoice is rock and humanChoice is scissors
+        // OR
+        // if computerChoice is paper and humanChoice is rock
+        // OR
+        // if computerChoice is scissors and humanChoice is paper
+        if ((computerAction == "Rock" && humanAction == "Scissors")
+            || (computerAction == "Paper" && humanAction == "Rock")
+            || (computerAction == "Scissors" && humanAction == "Paper")) {
+            //winner is computerChoice
+            winner = "Computer";
+
+            //else if computerChoice is not equal to humanChoice
+        } else if (computerAction != humanAction) {
+            //winner is humanChoice
+            winner = "Human";
+        }
+
+        //if winner is humanChoice
+        if (winner == "Human") {
+            //output "You win! 'humanChoice' beats 'computerChoice'."
+            console.log(`You win! ${humanAction} beats ${computerAction}.`);
+            //humanScore is incremented
+            humanScore = humanScore + 1;
+            //if winner is computerChoice
+        } else if (winner == "Computer") {
+            //output "You lose! 'computerChoice beats 'humanChoice'."
+            console.log(`You lose! ${computerAction} beats ${humanAction}.`);
+            //computerScore is incremented
+            computerScore = computerScore + 1;
+        } else {
+            console.log(`Noone wins! Both are ${humanAction}.`);
+        }
     }
 
-    console.log(getHumanChoice());
+    //loop five times
+    for (let i = 0; i < 5; i++) {
+        //run getComputerChoice
+        let computerSelection = getComputerChoice();
+        //run getHumanChoice
+        let humanSelection = getHumanChoice();
+        //call playRound
+        playRound(humanSelection, computerSelection);
+    }
+}
+
+//Run main program
+playGame();
+
 
